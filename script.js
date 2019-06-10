a7ba1e96ee09ba05cfc221502e261914b0960a45
Danny Barker <barkerdanny17@yahoo.com>
2019-06-10 15:48:55 -0500

    hopefully added API right

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-var name = prompt("What is your name?");
-if (name === name) {
-    alert("Hi " + name);
-}
+let btn = document.querySelector('#create_appointment');
+btn.addEventListener('click', () => {
+    let submitted_email = document.querySelector('#email').value
+    fetch (`https://api.towerdata.com/v5/ev?email=${submitted_email}&api_key=${keys.email_key}`)
+    .then( email => email.json())
+    .then( checkEmail => {
+        if (checkEmail.email_validation.status !== "valid") {
+            alert("Invalid Email!")
+        }  
+    })
+
+})
